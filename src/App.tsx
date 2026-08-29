7b1f4726136db27118a74605f46c6842432db660
Sashank Parajuli <shanks@Sashanks-Mac-mini.local>
2026-08-30 01:05:30 +0545

    Initial template

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Board from "./components/board.tsx"
+
+export default function App(){
+    return(
+      <Board/>
+    )
+}
---
+++ src/components/board.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Board(){
+    return(
+        <h2 className="text-2xl">
+            Chess
+        </h2>
+    )
+}
