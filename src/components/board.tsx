c0a7e8d01e86913c48df67f1cd12f810311a6ddd
Sashank Parajuli <shanks@Sashanks-Mac-mini.local>
2026-08-30 23:50:09 +0545

    feat : add chess board and pieces icons on the board

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,9 +1,31 @@
-
+// import { ChessQueen } from 'lucide-react';
 
 export default function Board(){
+
+     const squares = [
+    ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"],
+    ["♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙"],
+    ["♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"],
+  ];
+
     return(
-        <h2 className="text-2xl">
-            Chess
-        </h2>
+        <div>
+            <h2 className="text-2xl">
+                Chess
+            </h2>
+        
+            {squares.map((_,i) => (
+                <div className=" flex flex-row">{squares.map((_,j) => (
+                    <div key = {`${i}-${j}`} className= {` ${(i+j)%2 != 0 ? "bg-green-800" : "bg-white"} border border-black h-16 w-16`}>{squares[i][j]}</div>
+                ))}
+                </div>
+            ))}
+            
+        </div>
     )
 }
