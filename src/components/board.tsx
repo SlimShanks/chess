ad2ab2b9d5176c8cd15984a99b55ba9d2b7ec170
Sashank Parajuli <sasankparajuli@gmail.com>
2026-08-31 23:02:10 +0545

    feat : add event handler on each chess block

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,5 +1,7 @@
 // import { ChessQueen } from 'lucide-react';
-
+function handleClick(a : number, b : number){
+    console.log(a + "is clicked" + b + "is clicked")
+}
 export default function Board(){
 
      const squares = [
@@ -21,11 +23,13 @@ export default function Board(){
         
             {squares.map((_,i) => (
                 <div className=" flex flex-row">{squares.map((_,j) => (
-                    <div key = {`${i}-${j}`} className= {` ${(i+j)%2 != 0 ? "bg-green-800" : "bg-white"} border border-black h-16 w-16`}>{squares[i][j]}</div>
+                    <div key = {`${i}-${j}`} onClick={()=>handleClick(i,j)} className= {` ${(i+j)%2 != 0 ? "bg-green-800" : "bg-white"} border border-black h-16 w-16`}>{squares[i][j]}</div>
                 ))}
                 </div>
             ))}
-            
+
         </div>
     )
 }
+
+
