b04d9f5f85f5f42502d0043a5719f84cdcb7fe08
Sashank Parajuli <sasankparajuli@gmail.com>
2026-09-02 23:31:36 +0545

    feat : add valid moves block for pawn pieces

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -8,26 +8,48 @@ export default function Board(){
     const squares = [
         ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"],
         ["♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"],
-        ["", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", ""],
+        ["",   "",  "",  "",  "",  "",  "", ""],
+        ["",   "",  "",  "",  "",  "",  "", ""],
+        ["",   "",  "",  "",  "",  "",  "", ""],
+        ["",   "",  "",  "",  "",  "",  "", ""],
         ["♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙"],
         ["♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"],
     ];
 
     const [square, setSquare]= useState(squares);
     const [active,setActive] = useState<[number, number] | null>(null);
+    const [moves, setMoves] = useState<[number, number][]>([]);
 
     function handleClick(i : number, j : number){
-        console.log(i + "is clicked" + j + "is clicked")
-
+        setMoves([])
         if(active && active[0] == i && active[1] == j){
+            setMoves([])
             setActive(null)
+            
+        }else if(square[i][j] == ""){
+            setActive(null)
+            setMoves([])
         }else{
-            setActive([i,j]);
-        }
+            setActive([i,j])           
 
+            if(square[i][j] == "♙"){
+                let num: [number, number][] = [];
+                for(let val =1; val <= 2; val++){
+                    let temp = i-val;
+                    num.push([temp,j]);
+                }
+                setMoves(num)
+            }else if(square[i][j] == "♟"){
+                let num: [number, number][] = [];
+                for(let val =1; val <= 2; val++){
+                    let temp = i+val;
+                    num.push([temp,j]);
+                }
+                setMoves(num)
+            }
+        }
+    
+        
     }
     return(
         <div>
@@ -37,7 +59,14 @@ export default function Board(){
         
             {square.map((_,i) => (
                 <div className=" flex flex-row">{squares.map((_,j) => (
-                    <div key = {`${i}-${j}`} onClick={()=>handleClick(i,j)} className= {`${active && active[0] == i && active[1] == j  ? "bg-purple-600" : ""}  ${(i+j)%2 != 0 ? "bg-green-800" : "bg-white"} border border-black h-16 w-16`}>{square[i][j]}</div>
+                    <div key = {`${i}-${j}`} 
+                        onClick={()=>handleClick(i,j)} 
+                        className= {`${active && active[0] == i && active[1] == j  ? "bg-purple-600" : ""} 
+                                    ${active &&  moves.some(([x, y]) => x === i && y === j) ? "bg-purple-600" : ""}
+                                    ${(i+j)%2 != 0 ? "bg-green-800" : ""} 
+                                    border border-black h-16 w-16`}>
+                        {square[i][j]}
+                    </div>
                 ))}
                 </div>
             ))}
